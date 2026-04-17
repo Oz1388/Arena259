85ff75d4eb30b24828db2818684415abc429ca6e
Oz <perezfloreso@sou.edu>
2026-04-17 15:25:42 -0700

    added random attack and random target select

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,51 @@
+#include <iostream>
+#include <vector>
 #include "arena.h"
 #include "creature.h"
 #include <cstdlib>
 #include <ctime>
+#include "viper.h"
+#include "segoblin.h"
+#include "giantleech.h"
+
+using namespace std;
+
 
 
 
 int main()
 {
-    srand(time(0)); // begins random
-    Creature c1("Alpha", 100, 15);
-    Creature c2("Beta", 100, 12);
+    srand(time(0)); // begins randomness
 
-    Arena::battle(c1, c2);
+    vector<Creature> creatures;
+    // Create some creatures
+    creatures.push_back(makeViper());
+    creatures.push_back(makeSEGoblin());
+    creatures.push_back(makeGiantleech());
 
     return 0;
+
+int turn = 1;
+
+// loop until 1 creature left alive
+while (true)
+{
+    cout << "\nTurn " << turn << endl;
+
+    // count alive creatures
+    int aliveCount = 0;
+    for (int i = 0; i < creatures.size(); i++)
+    {
+        if (creatures[i].isAlive())
+            aliveCount++;
+    }
+
+    // stop if only one left
+    if (aliveCount <= 1)
+        break;
+
+    turn++;
 }
+
+}
+
